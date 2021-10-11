2d23a47c8763246564cf7cb5777fe3ce89a3f40a
nikenrhmdni <nikenrhmdni09@gmail.com>
2021-10-11 15:50:42 +0700

    p

---
+++ app/src/main/java/com/example/grocerytogo/PesananBatal.java
@@ -0,0 +1,14 @@
+package com.example.grocerytogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PesananBatal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesanan_batal);
+    }
+}
---
+++ app/src/main/java/com/example/grocerytogo/SedangMemesan.java
@@ -0,0 +1,14 @@
+package com.example.grocerytogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SedangMemesan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sedang_memesan);
+    }
+}
